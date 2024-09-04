7f7ffdc4cd6f8203108ba26ac6bd691def3e447b
tarickali <tarickali97@gmail.com>
2024-09-04 09:41:18 -0400

    Added target network with soft updates for DQN Agent

--- agents/agents/dqn.py
+++ agents/agents/dqn.py
@@ -2,26 +2,31 @@ import numpy as np
 import torch
 
 from agents.core.types import Observation, Action, Reward, Terminal, Transition
-from agents.core import Memory, Network
+from agents.core import Memory
+from agents.networks import QNetwork
 from agents.utils import unwrap_transitions
 
 
 class QAgent:
     def __init__(
         self,
-        network: Network,
+        network: QNetwork,
         memory: Memory,
         gamma: float = 0.99,
+        tau: int = 0.005,
         batch_size: int = 32,
     ) -> None:
-        self.network = network
+        self.behavior_network = network
         self.memory = memory
 
+        self.target_network = network.clone()
+
         self.gamma = gamma
+        self.tau = tau
         self.batch_size = batch_size
 
     def act(self, observation: Observation) -> Action:
-        return self.network.select(observation)
+        return self.behavior_network.select(observation)
 
     def learn(self) -> None:
         if self.memory.size < self.batch_size:
@@ -33,20 +38,34 @@ class QAgent:
         )
 
         # Get the action values of observations with the taken actions
-        # FIXME: does not work if output is continuous and not multiclass
-        Q_eval = self.network.compute(observations)[
+        Q_eval = self.behavior_network.compute(observations)[
             np.arange(self.batch_size, dtype=np.int32), actions
         ]
 
         # Get the action values of next observations
-        Q_prime = self.network.compute(next_observations)
+        with torch.no_grad():
+            Q_prime = self.target_network.compute(next_observations)
+
         Q_prime[terminals] = 0.0
 
         # Compute the target value to update the Q values
         Q_targets = rewards + self.gamma * torch.max(Q_prime, dim=1)[0]
 
-        # Update the network
-        self.network.update(Q_eval, Q_targets)
+        # Ensure that Q_targets and Q_eval have the same dtype
+        Q_targets = Q_targets.to(Q_eval.dtype)
+
+        # Update the behavior network
+        self.behavior_network.update(Q_eval, Q_targets)
+
+        # Update the target network
+        target_network_state_dict = self.target_network.model.state_dict()
+        behavior_network_state_dict = self.behavior_network.model.state_dict()
+
+        for key in behavior_network_state_dict:
+            target_network_state_dict[key] = behavior_network_state_dict[
+                key
+            ] * self.tau + target_network_state_dict[key] * (1 - self.tau)
+        self.target_network.model.load_state_dict(target_network_state_dict)
 
     def remember(
         self,
--- agents/networks/q_network.py
+++ agents/networks/q_network.py
@@ -1,12 +1,16 @@
+from __future__ import annotations
+import copy
+
 import numpy as np
 import torch
 
 from gymnasium import Space
 
 from agents.core.types import Observation, Action
+from agents.core import Network
 
 
-class QNetwork:
+class QNetwork(Network):
     def __init__(
         self,
         model: torch.nn.Module,
@@ -36,20 +40,34 @@ class QNetwork:
         if np.random.random() < self.epsilon:
             return torch.tensor(self.action_space.sample())
         else:
-            # Get q_values for each action
-            Qs = self.model.forward(observation)
-            # Get the greedy action from Qs
-            # TODO: what if a batch of observations are given? what if actions are multi-dimensional?
-            # FIXME: does not work if output is continuous and not multiclass
+            with torch.no_grad():
+                # Get q_values for each action
+                Qs = self.model.forward(observation)
+                # Get the greedy action from Qs
+                # TODO: what if a batch of observations are given? what if actions are multi-dimensional?
             return Qs.argmax(axis=-1)
 
     def update(self, prediction: torch.Tensor, target: torch.Tensor) -> None:
         self.optimizer.zero_grad()
-        # FIXME: make sure that prediction and target have the same dtype
-        loss = self.loss.forward(prediction, target.to(prediction.dtype))
+        loss = self.loss.forward(prediction, target)
         loss.backward()
+        torch.nn.utils.clip_grad_value_(self.model.parameters(), 100)
         self.optimizer.step()
 
         # TODO: Should this be done when learning or when acting?
         # Reduce epsilon
         self.epsilon = max(self.epsilon_end, self.epsilon - self.epsilon_decay)
+
+    def clone(self) -> QNetwork:
+        clone = copy.deepcopy(self)
+        # TODO: Do you need this if you are doing a deepcopy?
+        clone.model.load_state_dict(self.model.state_dict())
+        return clone
+
+    def freeze(self) -> None:
+        for param in self.model.parameters():
+            param.requires_grad = False
+
+    def unfreeze(self) -> None:
+        for param in self.model.parameters():
+            param.requires_grad = True
